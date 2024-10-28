d56334b28c40c9e51e8c8d3b567b36038ecbfc1f
Oğulcan Dursun <31250758+Rhekax@users.noreply.github.com>
2024-10-28 21:24:34 +0300

    Add files via upload
    Get next line seems done

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdursun <mdursun@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 20:59:33 by mdursun           #+#    #+#             */
+/*   Updated: 2024/10/28 20:59:33 by mdursun          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strncpy(char *dst, char *src, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < len && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	return (dst);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	char	*string;
+	size_t	i;
+
+	i = 0;
+	string = (char *) s;
+	while (i < n)
+	{
+		string[i] = 0;
+		i++;
+	}
+}
